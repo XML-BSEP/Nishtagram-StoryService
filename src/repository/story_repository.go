2ffc1bce661514e09b3c378a4cb1bfe2c1fab565
jelena-vlajkov <vlajkovj31@gmail.com>
2021-07-07 06:04:05 +0200

    :bug: Fixed minor bugs with links

--- src/repository/story_repository.go
+++ src/repository/story_repository.go
@@ -11,14 +11,14 @@ import (
 )
 
 const (
-	CreateStoryTable     = "CREATE TABLE IF NOT EXISTS story_keyspace.Stories (id text, profile_id text, image text, timestamp timestamp, mentions list<text>, close_friends boolean, type text, location_name text, longitude double, latitude double, deleted boolean, PRIMARY KEY (profile_id, id));"
-	InsertIntoStoryTable = "INSERT INTO story_keyspace.Stories (id, profile_id, image, timestamp, mentions, close_friends, type, location_name, longitude, latitude, deleted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
+	CreateStoryTable     = "CREATE TABLE IF NOT EXISTS story_keyspace.Stories (id text, profile_id text, image text, timestamp timestamp, mentions list<text>, close_friends boolean, type text, location_name text, longitude double, latitude double, deleted boolean,, is_campaign boolean, campaign_id text, link text,  PRIMARY KEY (profile_id, id));"
+	InsertIntoStoryTable = "INSERT INTO story_keyspace.Stories (id, profile_id, image, timestamp, mentions, close_friends, type, location_name, longitude, latitude, deleted, is_campaign, campaign_id, link) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"
 	GetAllStoryByUser    = "SELECT id, profile_id, image, timestamp, close_friends, type, mentions, location_name, latitude, longitude FROM story_keyspace.Stories WHERE profile_id = ? AND id = ? AND deleted = false;"
 	DeleteStory          = "UPDATE story_keyspace.Stories SET deleted = ? WHERE profile_id = ? AND id = ?;"
-	GetStoryById         = "SELECT id, profile_id, image, timestamp, close_friends, type, mentions, location_name, latitude, longitude, deleted FROM story_keyspace.Stories WHERE profile_id = ? AND id = ?;"
+	GetStoryById         = "SELECT id, profile_id, image, timestamp, close_friends, type, mentions, location_name, latitude, longitude, deleted, is_campaign, campaign_id, link FROM story_keyspace.Stories WHERE profile_id = ? AND id = ?;"
 	SeeIfExists          = "SELECT count(*) FROM story_keyspace.Stories WHERE profile_id = ? AND id = ?;"
 	GetMediaFromId 		 = "SELECT image FROM story_keyspace.Stories WHERE profile_id = ? AND id = ?;"
-	GetStoriesByUserId         = "SELECT id, profile_id, image, timestamp, close_friends, type, mentions, location_name, latitude, longitude, deleted FROM story_keyspace.Stories WHERE profile_id = ?;"
+	GetStoriesByUserId         = "SELECT id, profile_id, image, timestamp, close_friends, type, mentions, location_name, latitude, longitude, deleted, is_campaign, campaign_id, link FROM story_keyspace.Stories WHERE profile_id = ?;"
 
 )
 
@@ -38,18 +38,18 @@ type storyRepository struct {
 
 func (s storyRepository) GetStoryByAdmin(ctx context.Context, userId string, postId string) (dto.StoryDTO, error) {
 	var location domain.Location
-	var id, profileId, image, storyType, locationName string
+	var id, profileId, image, storyType, locationName,campaignId, link string
 	var mentions []string
 	var latitude, longitude float64
 	var timestamp time.Time
-	var closeFriends bool
+	var closeFriends, isCampaign bool
 	var deleted bool
 	iter := s.cassandraClient.Query(GetStoryById, userId, postId).Iter().Scanner()
 	//id, profile_id, image, timestamp, close_friends, type, mentions, location_name, latitude, longitude
 	for iter.Next() {
-		iter.Scan(&id, &profileId, &image, &timestamp, &closeFriends, &storyType, &mentions, &locationName, &latitude, &longitude, &deleted)
+		iter.Scan(&id, &profileId, &image, &timestamp, &closeFriends, &storyType, &mentions, &locationName, &latitude, &longitude, &deleted, &isCampaign, &campaignId, &link)
 		location = domain.NewLocation(locationName, latitude, longitude)
-		return dto.NewStoryDTO(id, profileId, mentions, domain.Media{Path: image, Timestamp: timestamp}, storyType, location, timestamp, closeFriends), nil
+		return dto.NewStoryDTO(id, profileId, mentions, domain.Media{Path: image, Timestamp: timestamp}, storyType, location, timestamp, closeFriends, isCampaign, campaignId, link), nil
 
 	}
 	return dto.StoryDTO{}, fmt.Errorf("no such story")
@@ -57,20 +57,20 @@ func (s storyRepository) GetStoryByAdmin(ctx context.Context, userId string, pos
 
 func (s storyRepository) GetAllStoriesById(ctx context.Context, userId string) ([]dto.StoryDTO, error) {
 	var location domain.Location
-	var id, profileId, image, storyType, locationName string
+	var id, profileId, image, storyType, locationName, campaignId, link string
 	var mentions []string
 	var latitude, longitude float64
 	var timestamp time.Time
-	var closeFriends bool
+	var closeFriends, isCampaign bool
 	var retVal []dto.StoryDTO
 	var deleted bool
 	iter := s.cassandraClient.Query(GetStoriesByUserId, userId).Iter().Scanner()
 	//id, profile_id, image, timestamp, close_friends, type, mentions, location_name, latitude, longitude
 	for iter.Next() {
-		iter.Scan(&id, &profileId, &image, &timestamp, &closeFriends, &storyType, &mentions, &locationName, &latitude, &longitude, &deleted)
+		iter.Scan(&id, &profileId, &image, &timestamp, &closeFriends, &storyType, &mentions, &locationName, &latitude, &longitude, &deleted, &isCampaign, &campaignId, &link)
 		if !deleted {
 			location = domain.NewLocation(locationName, latitude, longitude)
-			retVal = append(retVal, dto.NewStoryDTO(id, profileId, mentions, domain.Media{Path: image, Timestamp: timestamp}, storyType, location, timestamp, closeFriends))
+			retVal = append(retVal, dto.NewStoryDTO(id, profileId, mentions, domain.Media{Path: image, Timestamp: timestamp}, storyType, location, timestamp, closeFriends, isCampaign, campaignId, link))
 		}
 
 	}
@@ -85,19 +85,19 @@ func (s storyRepository) SeeIfExists(ctx context.Context, userId string, storyId
 
 func (s storyRepository) GetStoryById(ctx context.Context, userId string, postId string) (dto.StoryDTO, error) {
 	var location domain.Location
-	var id, profileId, image, storyType, locationName string
+	var id, profileId, image, storyType, locationName, campaignId, link string
 	var mentions []string
 	var latitude, longitude float64
 	var timestamp time.Time
-	var closeFriends bool
+	var closeFriends, isCampaign bool
 	var deleted bool
 	iter := s.cassandraClient.Query(GetStoryById, userId, postId).Iter().Scanner()
 	//id, profile_id, image, timestamp, close_friends, type, mentions, location_name, latitude, longitude
 	for iter.Next() {
-		iter.Scan(&id, &profileId, &image, &timestamp, &closeFriends, &storyType, &mentions, &locationName, &latitude, &longitude, &deleted)
+		iter.Scan(&id, &profileId, &image, &timestamp, &closeFriends, &storyType, &mentions, &locationName, &latitude, &longitude, &deleted, &isCampaign, &campaignId, &link)
 		if !deleted {
 			location = domain.NewLocation(locationName, latitude, longitude)
-			return dto.NewStoryDTO(id, profileId, mentions, domain.Media{Path: image, Timestamp: timestamp}, storyType, location, timestamp, closeFriends), nil
+			return dto.NewStoryDTO(id, profileId, mentions, domain.Media{Path: image, Timestamp: timestamp}, storyType, location, timestamp, closeFriends, isCampaign, campaignId, link), nil
 		}
 
 	}
@@ -111,7 +111,7 @@ func (s storyRepository) AddStory(ctx context.Context, story domain.Story) error
 		mentions = append(mentions, st.Id)
 	}
 	err := s.cassandraClient.Query(InsertIntoStoryTable, story.Id, story.Profile.Id, story.Media.Path, story.Timestamp, mentions, story.CloseFriends,
-		story.StoryType.Type, story.Location.Location, story.Location.Longitude, story.Location.Latitude, false).Exec()
+		story.StoryType.Type, story.Location.Location, story.Location.Longitude, story.Location.Latitude, false, story.IsCampaign, story.CampaignId, story.Link).Exec()
 	if err != nil {
 		s.logger.Logger.Errorf("error while adding story for user %v, error: %v\n", story.Profile.Id, err)
 		return fmt.Errorf("server error")
